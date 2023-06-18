d815dd6822c056494931e8eeaa46c94d29db261a
Shubham Patel <shubhampatel518@gmail.com>
2023-06-18 14:40:36 +0530

    sync

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,11 +1,13 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 40,
+        width: dbg!(30 * scale),
         height: 55
     };
 
@@ -13,6 +15,8 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+
+    dbg!(&rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
