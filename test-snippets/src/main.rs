60fffac358d5ca33505e26dfea99461c4d7d2b32
Shubham Patel <shubhampatel518@gmail.com>
2023-12-05 12:24:53 +0530

    sync

--- test-snippets/src/main.rs
+++ test-snippets/src/main.rs
@@ -1,23 +1,50 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+// }
+
+// fn main() {
+
+//     let user1 = User {
+//         email: String::from("someone@example.com"),
+//         username: String::from("someusername123"),
+//         active: true,
+//         sign_in_count: 1,
+//     };
+
+//     let user2 = User {
+//         email: String::from("another@example.com"),
+//         ..user1
+//     };
+
+//     println!("{} and {}", user1.email, user2.email);
+// }
+
+fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
+    if str1.len() > str2.len() {
+        return str1;
+    }
+    str2
 }
 
-fn main() {
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
 
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
 
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
+    &s[..]
+}
 
-    println!("{} and {}", user1.email, user2.email);
+fn main () {
+    let s1 = String::from("looooongest");
+    let s2 = String::from("shortest 1");
+
+    print!("{}\n", longest(&s1, &s2));
+    println!("The first word is: {}", first_word(&s2));
 }
