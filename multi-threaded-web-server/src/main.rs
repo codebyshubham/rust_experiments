e580fbbd944f8b1b9877d1c425c80825b066f3f9
Shubham Patel <shubhampatel518@gmail.com>
2023-12-13 16:22:51 +0530

    multi thread support

--- multi-threaded-web-server/src/main.rs
+++ multi-threaded-web-server/src/main.rs
@@ -5,8 +5,10 @@ use std::{
     net::{TcpListener, TcpStream},
     time::Duration
 };
+use multi_threaded_web_server::ThreadPool;
 
 fn main() {
+    let pool = ThreadPool::new(4);
     let port = 8080;
     let ip = "127.0.0.1";
     let address = format!("{}:{}", ip, port);
@@ -17,7 +19,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -28,7 +32,7 @@ fn handle_connection(mut stream: TcpStream) {
     let (status_line, content_file) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
+            thread::sleep(Duration::from_secs(2));
             ("HTTP/1.1 200 OK", "hello.html")
         },
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
